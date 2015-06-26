4e6cfc120644829bdce607d3cff31677a6cbbf43
CoderGMLee <LGM@GM.local>
2015-06-26 09:41:31 +0800

    GPUImageDemo

---
+++ GPUImageDemo/ViewController.h
@@ -0,0 +1,22 @@
+//
+//  ViewController.h
+//  GPUImageDemo
+//
+//  Created by LGM on 15/6/25.
+//  Copyright (c) 2015年 GM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "GPUImage.h"
+@interface ViewController : UIViewController
+{
+    GPUImagePicture *sourcePicture;
+    GPUImageOutput<GPUImageInput> *sepiaFilter, *sepiaFilter2;
+    
+    UISlider *imageSlider;
+}
+
+
+
+@end
+
